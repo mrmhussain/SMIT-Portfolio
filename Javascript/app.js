49879f761364addfa4d3861677d180571ccf7041
mrmhussain <mhussainmyousuf12@gmail.com>
2024-05-09 14:58:20 +0500

    javascript

---
+++ Javascript/app.js
@@ -0,0 +1,13 @@
+// alert method
+// alert("Hello World!");
+
+// Variable
+var fullName = "Muhammad Hussain";
+alert(fullName);
+
+var occupation;
+occupation = "Web Developer";
+alert(occupation);
+
+var mobileNumber = "+923160108183";
+alert (mobileNumber);
